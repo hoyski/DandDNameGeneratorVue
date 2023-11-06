434f4b3313174ea43f966e6487549a43b7f0ca24
judehoy <blackbeltjude@gmail.com>
2023-11-06 17:41:35 -0500

    Added randomized picking of races

--- src/components/TavernsList.vue
+++ src/components/TavernsList.vue
@@ -1,4 +1,7 @@
 <template>
+  <div class="generator">
+    <button @click="generateTheming">Generator</button>
+  </div>
   <div class="taverns-container">
     <div v-if="generating">Generating...</div>
     <button v-else @click="generateNewTavern">Generate Tavern</button>
@@ -18,29 +21,48 @@
         </tr>
       </thead>
       <tbody>
-        <tr
-          class="tavern-row"
-        >
+        <tr class="tavern-row">
 
-        <td>{{ tavern.Name }}</td>
-        <td>{{ tavern.Size }}</td>
-        <td>{{ tavern.Quality }}</td>
-        <td>{{ tavern.Bartender.Name }}</td>
-        <td>{{ tavern.Bartender.Race }}</td>
-        <td>{{ tavern.Bartender.Gender }}</td>
-        <td>{{ tavern.Bartender.Age }}</td>
+          <td>{{ tavern.Name }}</td>
+          <td>{{ tavern.Size }}</td>
+          <td>{{ tavern.Quality }}</td>
+          <td>{{ tavern.Bartender.Name }}</td>
+          <td>{{ tavern.Bartender.Race }}</td>
+          <td>{{ tavern.Bartender.Gender }}</td>
+          <td>{{ tavern.Bartender.Age }}</td>
 
 
         </tr>
       </tbody>
     </table>
- <p>
+    <div class="races">
+      <div class="race" v-for="curRace, index in races" v-bind:key="index">
+        <div>
+          {{ curRace.race }}
+        </div>
+        <div>
+          {{ curRace.weight }}
+        </div>
+        <div>
+          <button v-on:click="removeRace(index)">Remove</button>
+        </div>
+      </div>
+      <div class="newRace race">
+        <input type="text" v-model="newRace.race">
+        <br />
+        <input type="number" v-model="newRace.weight">
+        <br />
+        <button v-on:click="addRace">+</button>
+      </div>
+    </div>
+    <p>
 
- </p>
+    </p>
   </div>
 </template>
 
 <script>
+
 import OpenAI from "openai";
 
 export default {
@@ -48,62 +70,84 @@ export default {
     return {
       generating: false,
       tavern: {
-  "Name": "",
-  "Size": 0,
-  "Quality": "",
-  "Bartender": {
-    "Name": "",
-    "Race": "",
-    "Gender": "",
-    "Age": 0
-  }
-},
-      setting: 'Fantasy'
+        "Name": "",
+        "Size": 0,
+        "Quality": "",
+        "Bartender": {
+          "Name": "",
+          "Race": "",
+          "Gender": "",
+          "Age": 0
+        }
+      },
+      setting: 'Fantasy',
+      tavernSmells: [
+        "Pine",
+        "Smoke",
+        "Sweat",
+        "Hard Liquor",
+        "Spruce",
+        "Rain",
+        "Fresh Cut Grass",
+        "Cooked Meat",
+        "Wine",
+        "Burnt Meat",
+        "Rubber",
+        "Sulfur",
+        "Metal",
+        "Wheat",
+        "Blood",
+        "Oil",
+        "Fish",
+        "Salt",
+        "Baked Goods",
+        "Pickles",
+        "Flowers",
+        "Strawberry",
+        "Banana",
+        "Grape",
+        "Watermelon",
+        "Kiwi",
+        "Mango",
+        "Milk",
+        "Birch",
+        "Oak",
+        "Aspen",
+        "Acacia",
+        "Potato"
+      ],
+      selectedSmell: '',
+      races: [
+        {
+          race: 'human',
+          weight: 50
+        },
+        {
+          race: 'elf',
+          weight: 20
+        },
+        {
+          race: 'dwarf',
+          weight: 12
+        },
+        {
+          race: 'dragonborn',
+          weight: 14
+        }
+      ],
+      newRace: {
+        race: '',
+        weight: 0
+      }
     }
   },
   components: {},
   methods: {
     async generateNewTavern() {
 
-      let tavernSmells = [
-        "Pine",
-  "Smoke",
-  "Sweat",
-  "Hard Liquor",
-  "Spruce",
-  "Rain",
-  "Fresh Cut Grass",
-  "Cooked Meat",
-  "Wine",
-  "Burnt Meat",
-  "Rubber",
-  "Sulfur",
-  "Metal",
-  "Wheat",
-  "Blood",
-  "Oil",
-  "Fish",
-  "Salt",
-  "Baked Goods",
-  "Pickles",
-  "Flowers",
-  "Strawberry",
-  "Banana",
-  "Grape",
-  "Watermelon",
-  "Kiwi",
-  "Mango",
-  "Milk",
-  "Birch",
-  "Oak",
-  "Aspen",
-  "Acacia",
-  "Potato"
-      ]
-
-      let smellNumber = Math.floor(Math.random() * tavernSmells.length);
-
       this.generating = true;
+
+      this.generateTheming();
 
       const openai = new OpenAI({
         apiKey: process.env.OPENAI_API_KEY,
@@ -121,7 +165,7 @@ export default {
             role: "user",
             content: `Generate a tavern with the following properties and return the results as a JSON object.
 
-Tavern: Generate a fantasy tavern with the list of parameters and the following caveats of each parameter this tavern also has some unique properties so please keep those in mind when generating this tavern, this tavern smells of ${tavernSmells[smellNumber]}
+Tavern: Generate a fantasy tavern with the list of parameters and the following caveats of each parameter this tavern also has some unique properties so please keep those in mind when generating this tavern, this tavern smells of ${this.selectedSmell}
 
 Name - Name of said tavern (Caveat: no animal related names including mythical creatures)
 Size - Amount of people in the tavern (Caveat: come up with a random number 5 - 40)
@@ -155,13 +199,53 @@ Use this as the framework for your response
 
       //this.$store.commit("ADD_TAVERN", tavernInfo);
 
-
-
       this.generating = false;
 
     },
+    generateTheming() {
+      let pickedRace = this.pickWeightedRace();
+      let smellNumber = Math.floor(Math.random() * this.tavernSmells.length);
+      this.selectedSmell = this.tavernSmells[smellNumber];
+      console.log(this.selectedSmell);
+      console.log(pickedRace.race);
+
+    },
+    pickWeightedRace() {
+    let raceTotal = 0;
+    for (let i = 0; i < this.races.length; i++) {
+      raceTotal += this.races[i].weight;
+    }
+
+    const raceThreshold = Math.floor(Math.random() * raceTotal);
+
+    raceTotal = 0;
+    for (let i = 0; i < this.races.length - 1; i++) {
+      raceTotal += this.races[i].weight;
+
+      if (raceTotal >= raceThreshold) {
+        return this.races[i];
+      }
+    }
+    return this.races[this.races.length - 1];
   },
-};
+  addRace() {
+    if (this.newRace.race == '') {
+      return;
+    }
+    let newRaceCopy = {};
+    newRaceCopy.race = this.newRace.race;
+    newRaceCopy.weight = this.newRace.weight;
+    this.races.push(newRaceCopy);
+    this.newRace.race = '';
+    this.newRace.weight = 0;
+  },
+  removeRace(index) {
+    console.log("Remove Race " + index);
+    this.races.splice(index, 1);
+  }
+  },
+}
+
 </script>
 
 <style scoped>
@@ -170,14 +254,26 @@ table {
   border-spacing: 0;
 }
 
-thead > tr > td {
+thead>tr>td {
   font-weight: bold;
 }
 
 tr:nth-child(even) {
   background-color: #f0f0f0;
 }
-.tavernName{
+
+.tavernName {
   font-size: 40px;
+}
+
+.races {
+  display: flex;
+}
+
+.race {
+  border: 1px solid #000000;
+  margin: 20px;
+  padding: 5px;
+  width: 14%;
 }
 </style>
